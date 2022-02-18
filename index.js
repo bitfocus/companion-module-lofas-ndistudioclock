8f20f8850c49a05051d9c8ca203dac9048feaa62
Matteo Gheza <matteo.gheza07@gmail.com>
2022-02-18 23:57:55 +0100

    Run yarn format

--- index.js
+++ index.js
@@ -20,24 +20,24 @@ class instance extends instance_skel {
 	}
 
 	updateConfig(config) {
-		this.config = config;
-		this.stopPolling();
-		this.startPolling();
+		this.config = config
+		this.stopPolling()
+		this.startPolling()
 	}
 
 	init() {
 		this.status(this.STATE_OK)
 
-		this.setVariableDefinitions( [
+		this.setVariableDefinitions([
 			{
 				label: 'Countdown Text',
-				name: 'countdown_text'
+				name: 'countdown_text',
 			},
 			{
 				label: 'Countdown Time',
-				name: 'countdown_time'
-			}
-		] );
+				name: 'countdown_time',
+			},
+		])
 
 		debug = this.debug
 		log = this.log
@@ -57,54 +57,61 @@ class instance extends instance_skel {
 				id: 'port',
 				label: 'Port',
 				width: 6,
-				default: 13344
-			}
+				default: 13344,
+			},
 		]
 	}
 
 	// When module gets deleted
 	destroy() {
-		this.stopPolling();
+		this.stopPolling()
 	}
 
 	parseStates(data) {
-		this.states = {};
+		this.states = {}
 		data.forEach((el) => {
-			this.states[el.id] = "state" in el ? el.state : el.text;
-		});
-		this.setVariable('countdown_text', this.states.countdowntext);
-		this.setVariable('countdown_time', this.states.countdowntime);
-		this.checkFeedbacks('onair', 'enablecountdown', 'autoonair', '12hclock', 'transparent', 'showclockhands');
+			this.states[el.id] = 'state' in el ? el.state : el.text
+		})
+		this.setVariable('countdown_text', this.states.countdowntext)
+		this.setVariable('countdown_time', this.states.countdowntime)
+		this.checkFeedbacks('onair', 'enablecountdown', 'autoonair', '12hclock', 'transparent', 'showclockhands')
 	}
 
 	startPolling() {
-		this.system.emit('rest_poll_destroy', this.id);
-		this.system.emit('rest_poll_get', this.id, parseInt(this.pollingInterval), `http://${this.config.host}:${this.config.port}/api/getstates`, (err, pollInstance) => {
-			this.status(this.STATUS_ERROR, 'Polling Failed');
-		}, (error, response) => {
-			if(error || typeof response.data !== 'object') {
-				this.status(this.STATUS_ERROR, 'States Request Failed');
-			} else {
-				this.status(this.STATUS_OK);
-				this.parseStates(response.data);
+		this.system.emit('rest_poll_destroy', this.id)
+		this.system.emit(
+			'rest_poll_get',
+			this.id,
+			parseInt(this.pollingInterval),
+			`http://${this.config.host}:${this.config.port}/api/getstates`,
+			(err, pollInstance) => {
+				this.status(this.STATUS_ERROR, 'Polling Failed')
+			},
+			(error, response) => {
+				if (error || typeof response.data !== 'object') {
+					this.status(this.STATUS_ERROR, 'States Request Failed')
+				} else {
+					this.status(this.STATUS_OK)
+					this.parseStates(response.data)
+				}
 			}
-		});
+		)
 	}
 
 	stopPolling() {
-		this.system.emit('rest_poll_destroy', this.id);
+		this.system.emit('rest_poll_destroy', this.id)
 	}
 
 	actions(system) {
 		this.setActions({
 			enable_onair: {
-				label: 'Enable ON AIR'
+				label: 'Enable ON AIR',
 			},
-            disable_onair: {
-				label: 'Disable ON AIR'
+			disable_onair: {
+				label: 'Disable ON AIR',
 			},
 			toggle_onair: {
-				label: 'Toggle ON AIR'
+				label: 'Toggle ON AIR',
 			},
 			set_countdown_text: {
 				label: 'Set countdown text',
@@ -112,9 +119,9 @@ class instance extends instance_skel {
 					{
 						type: 'textinput',
 						id: 'text',
-						label: 'Text'
-					}
-				]
+						label: 'Text',
+					},
+				],
 			},
 			set_countdown_time: {
 				label: 'Set countdown time',
@@ -122,108 +129,125 @@ class instance extends instance_skel {
 					{
 						type: 'textinput',
 						id: 'time',
-						label: 'Time (use format HH:MM:SS)'
-					}
-				]
+						label: 'Time (use format HH:MM:SS)',
+					},
+				],
 			},
-            enable_countdown: {
-				label: 'Enable countdown'
+			enable_countdown: {
+				label: 'Enable countdown',
 			},
-            disable_countdown: {
-				label: 'Disable countdown'
+			disable_countdown: {
+				label: 'Disable countdown',
 			},
 			toggle_countdown: {
-				label: 'Toggle countdown'
+				label: 'Toggle countdown',
 			},
-            enable_countdown_go_on_air_on_time: {
-				label: 'Go ON AIR when countdown finishes'
+			enable_countdown_go_on_air_on_time: {
+				label: 'Go ON AIR when countdown finishes',
 			},
-            disable_countdown_go_on_air_on_time: {
-				label: 'Disable going ON AIR when countdown finishes'
+			disable_countdown_go_on_air_on_time: {
+				label: 'Disable going ON AIR when countdown finishes',
 			},
 			toggle_countdown_go_on_air_on_time: {
-				label: 'Toggle going ON AIR when countdown finishes'
+				label: 'Toggle going ON AIR when countdown finishes',
 			},
-            enable_12hclock: {
-				label: 'Show clock in 12 hours format (AM/PM)'
+			enable_12hclock: {
+				label: 'Show clock in 12 hours format (AM/PM)',
 			},
-            disable_12hclock: {
-				label: 'Show clock in 24 hours format'
+			disable_12hclock: {
+				label: 'Show clock in 24 hours format',
 			},
 			toggle_12hclock: {
-				label: 'Toggle clock in 24 hours format'
+				label: 'Toggle clock in 24 hours format',
 			},
 			enable_transparent_background: {
-				label: 'Enable transparent background'
+				label: 'Enable transparent background',
 			},
 			disable_transparent_background: {
-				label: 'Disable transparent background'
+				label: 'Disable transparent background',
 			},
 			toggle_transparent_background: {
-				label: 'Toggle transparent background'
+				label: 'Toggle transparent background',
 			},
 			enable_show_clock_hands: {
-				label: 'Show clock hands'
+				label: 'Show clock hands',
 			},
 			disable_show_clock_hands: {
-				label: 'Hide clock hands'
+				label: 'Hide clock hands',
 			},
 			toggle_show_clock_hands: {
-				label: 'Toggle clock hands'
-			}
+				label: 'Toggle clock hands',
+			},
 		})
 	}
 
 	action(action) {
-		let action_switch_feature = /(enable|disable|toggle)_(onair|countdown_go_on_air_on_time|countdown|12hclock|transparent_background|show_clock_hands)/gm.exec(action.action);
+		let action_switch_feature =
+			/(enable|disable|toggle)_(onair|countdown_go_on_air_on_time|countdown|12hclock|transparent_background|show_clock_hands)/gm.exec(
+				action.action
+			)
 
-		if(action_switch_feature) {
-			let feature = action_switch_feature[2];
+		if (action_switch_feature) {
+			let feature = action_switch_feature[2]
 			let api_endpoint = feature
 				.replace('countdown_go_on_air_on_time', 'autoonair')
 				.replace('transparent_background', 'transparent')
 				.replace('countdown', 'enablecountdown')
-				.replace(/_/g, '');
+				.replace(/_/g, '')
 
-			let enable = action_switch_feature[1] == 'enable';
-			if(action_switch_feature[1] == 'toggle') {
-				enable = !this.states[api_endpoint];
+			let enable = action_switch_feature[1] == 'enable'
+			if (action_switch_feature[1] == 'toggle') {
+				enable = !this.states[api_endpoint]
 			}
-			let api_state = enable ? 'true' : 'false';
+			let api_state = enable ? 'true' : 'false'
 
-			this.system.emit('rest_get', `http://${this.config.host}:${this.config.port}/api/${api_endpoint}/${api_state}`, (error, params) => {
-				if(error) {
-					this.status(this.STATE_ERROR, 'Request error');
-				} else {
-					this.status(this.STATE_OK);
-					this.parseStates(params.data.currentState);
+			this.system.emit(
+				'rest_get',
+				`http://${this.config.host}:${this.config.port}/api/${api_endpoint}/${api_state}`,
+				(error, params) => {
+					if (error) {
+						this.status(this.STATE_ERROR, 'Request error')
+					} else {
+						this.status(this.STATE_OK)
+						this.parseStates(params.data.currentState)
+					}
 				}
-			});
-		} else if(action.action == 'set_countdown_text') {
-			this.system.emit('rest', `http://${this.config.host}:${this.config.port}/api/countdowntext`, action.options.text, (error, params) => {
-				if(error) {
-					this.status(this.STATE_ERROR, 'Request error');
-				} else {
-					this.status(this.STATE_OK);
-					this.parseStates(params.data.currentState);
+			)
+		} else if (action.action == 'set_countdown_text') {
+			this.system.emit(
+				'rest',
+				`http://${this.config.host}:${this.config.port}/api/countdowntext`,
+				action.options.text,
+				(error, params) => {
+					if (error) {
+						this.status(this.STATE_ERROR, 'Request error')
+					} else {
+						this.status(this.STATE_OK)
+						this.parseStates(params.data.currentState)
+					}
 				}
-			});
-		} else if(action.action == 'set_countdown_time') {
-			this.system.emit('rest', `http://${this.config.host}:${this.config.port}/api/countdowntime`, action.options.time, (error, params) => {
-				if(error) {
-					this.status(this.STATE_ERROR, 'Request error');
-				} else {
-					this.status(this.STATE_OK);
-					this.parseStates(params.data.currentState);
+			)
+		} else if (action.action == 'set_countdown_time') {
+			this.system.emit(
+				'rest',
+				`http://${this.config.host}:${this.config.port}/api/countdowntime`,
+				action.options.time,
+				(error, params) => {
+					if (error) {
+						this.status(this.STATE_ERROR, 'Request error')
+					} else {
+						this.status(this.STATE_OK)
+						this.parseStates(params.data.currentState)
+					}
 				}
-			});
+			)
 		}
 	}
 
 	feedbacks() {
 		const defaultStyle = {
 			color: this.rgb(0, 0, 0),
-			bgcolor: this.rgb(255, 0, 0)
+			bgcolor: this.rgb(255, 0, 0),
 		}
 		const defaultOptions = [
 			{
@@ -233,9 +257,9 @@ class instance extends instance_skel {
 				default: '1',
 				choices: [
 					{ id: 1, label: 'Enabled' },
-					{ id: 2, label: 'Disabled' }
-				]
-			}
+					{ id: 2, label: 'Disabled' },
+				],
+			},
 		]
 
 		this.setFeedbackDefinitions({
@@ -243,43 +267,43 @@ class instance extends instance_skel {
 				label: 'ON AIR',
 				type: 'boolean',
 				style: defaultStyle,
-				options: defaultOptions
+				options: defaultOptions,
 			},
 			enablecountdown: {
 				label: 'Countdown enabled',
 				type: 'boolean',
 				style: defaultStyle,
-				options: defaultOptions
+				options: defaultOptions,
 			},
 			autoonair: {
 				label: 'Auto ON AIR enabled',
 				type: 'boolean',
 				style: defaultStyle,
-				options: defaultOptions
+				options: defaultOptions,
 			},
 			'12hclock': {
 				label: 'Displaying 12 hours clock',
 				type: 'boolean',
 				style: defaultStyle,
-				options: defaultOptions
+				options: defaultOptions,
 			},
 			transparent: {
 				label: 'Using transparent background',
 				type: 'boolean',
 				style: defaultStyle,
-				options: defaultOptions
+				options: defaultOptions,
 			},
 			showclockhands: {
 				label: 'Showing clock hands',
 				type: 'boolean',
 				style: defaultStyle,
-				options: defaultOptions
-			}
-		});
+				options: defaultOptions,
+			},
+		})
 	}
 
 	feedback(feedback) {
-        return (feedback.options.state === 1) === this.states[feedback.type];
+		return (feedback.options.state === 1) === this.states[feedback.type]
 	}
 }
 exports = module.exports = instance
